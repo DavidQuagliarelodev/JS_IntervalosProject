72428b8150e4257c84f096626975139ce83dcf69
DavidQuagliarelodev <quagliarelodev@gmail.com>
2024-11-05 13:33:12 -0300

    --ficture enarmonics logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 const notas = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const equivalentes = {
+    "C#": "Db", "D#": "Eb", "F#": "Gb", "G#": "Ab", "A#": "Bb"
+};
+
 const intervalos = [
     "2ªm", "2ªM", "3ªm", "3ªM",
-    "4ªJ", "4ªAum", "5ªJ", "6ªAum",
+    "4ªJ", "5ªDim", "5ªJ", "5ªAum",
     "6ªm", "6ªM", "7ªm", "7ªM"
 ];
 const semitons = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11]; // Semitons para cada intervalo
@@ -16,7 +20,15 @@ notas.forEach((nota, notaIndex) => {
         
         // Calcula a resposta correta
         const respostaIndex = (notaIndex + semitons[intervaloIndex]) % 12;
-        answers.push(notas[respostaIndex]);
+        const respostaNota = notas[respostaIndex];
+        
+        // Adiciona a nota principal e sua enarmonia (se existir)
+        const respostaAlternativas = [respostaNota];
+        if (equivalentes[respostaNota]) {
+            respostaAlternativas.push(equivalentes[respostaNota]);
+        }
+        
+        answers.push(respostaAlternativas);
     });
 });
 
@@ -40,13 +52,13 @@ nextQuestionBtn.addEventListener("click", () => {
 
 const sendAnswerBtn = document.getElementById("sendAnswerBtn");
 sendAnswerBtn.addEventListener("click", () => {
-    const answerInput = document.getElementById("answerInput").value;
+    const answerInput = document.getElementById("answerInput").value.trim();
     
     if (questionResult != null) {
-        if (answerInput.trim() === answers[questionResult]) {
+        if (answers[questionResult].includes(answerInput)) {
             window.alert("Certo");
         } else {
-            window.alert(`Errado! A resposta correta é ${answers[questionResult]}`);
+            window.alert(`Errado! A resposta correta é ${answers[questionResult].join(" ou ")}`);
         }
     }
 });
